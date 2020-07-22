79d8b123380b7ae1e3d8b4f70742e08efc5bdebf
realjf <real_jf@hotmail.com>
2020-07-23 06:32:35 +0800

    updates

--- deploy-to-github.bat
+++ deploy-to-github.bat
@@ -1,23 +1,24 @@
+@echo off
 echo "Deploying updates to GitHub..."
 
-# update
+rem update
 git pull
 
-# init public
+rem init public
 rmdir /s public
 mkdir public
 git worktree prune
 
-# checking out gh-pages branch into public
+rem checking out gh-pages branch into public
 git worktree add -B gh-pages public origin/gh-pages
-# 利用hugo生成静态文件
+rem 利用hugo生成静态文件
 hugo -t m10c
-# 提交更新gh-pages branch
+rem 提交更新gh-pages branch
 cd public
 git add --all
 git commit -m "update"
 cd ..
-# 推送到仓库
+rem 推送到仓库
 git push origin gh-pages
 
 echo "done"
