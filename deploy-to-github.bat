6777d2b187b940cae4340787d73c45e66b676ccf
chenjiefeng <chenjiefeng@4399inc.com>
2020-09-07 18:22:17 +0800

    updates

--- deploy-to-github.bat
+++ deploy-to-github.bat
@@ -1,24 +1,24 @@
 @echo off
 echo "Deploying updates to GitHub..."
 
-rem update
+:: update
 git pull
 
-rem init public
+:: init public
 rmdir /s public
 mkdir public
 git worktree prune
 
-rem checking out gh-pages branch into public
+:: checking out gh-pages branch into public
 git worktree add -B gh-pages public origin/gh-pages
-rem 利用hugo生成静态文件
+:: 利用hugo生成静态文件
 hugo -t m10c
-rem 提交更新gh-pages branch
+:: 提交更新gh-pages branch
 cd public
 git add --all
 git commit -m "update"
 cd ..
-rem 推送到仓库
+:: 推送到仓库
 git push origin gh-pages
 
 echo "done"
