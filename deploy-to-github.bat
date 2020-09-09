682f2cefcfbc0551af58b5f8cb8ad2566be90e1f
chenjiefeng <chenjiefeng@4399inc.com>
2020-09-09 09:38:55 +0800

    updates

--- deploy-to-github.bat
+++ deploy-to-github.bat
@@ -11,14 +11,14 @@ git worktree prune
 
 rem checking out gh-pages branch into public
 git worktree add -B gh-pages public origin/gh-pages
-rem 利用hugo生成静态文件
+rem hugo generate static files
 hugo -t m10c
-rem 提交更新gh-pages branch
+rem commit updates to gh-pages branch
 cd public
 git add --all
 git commit -m "update"
 cd ..
-rem 推送到仓库
+rem push to repo
 git push origin gh-pages
 
 echo "done"
